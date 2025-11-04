84e45d36c675ca179a1afce4cc1d6b7cbbc2d5dc
Yara <yaramohamed1886757@icloud.com>
2025-11-04 17:43:40 +0200

    Initial commit

---
+++ lib/features/splash/view/splash_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Image.asset("assets/images/Splash.png", fit: BoxFit.cover),
+      ),
+    );
+  }
+}
